7de134d500a517de1ddb689ac2982a06f294a570
Henrik Teinelund <henrik.teinelund@gmail.com>
2018-06-29 16:30:16 +0200

    Implemented fetching version from pom.xml (when invoking flag -v)

--- src/main/java/org/teinelund/application/commandline/addbuilder/strategy/VersionStrategy.java
+++ src/main/java/org/teinelund/application/commandline/addbuilder/strategy/VersionStrategy.java
@@ -1,9 +1,26 @@
 package org.teinelund.application.commandline.addbuilder.strategy;
 
+import org.apache.maven.model.Model;
+import org.apache.maven.model.io.xpp3.MavenXpp3Reader;
+import org.codehaus.plexus.util.xml.pull.XmlPullParserException;
+
+import java.io.FileReader;
+import java.io.IOException;
+
 public class VersionStrategy implements Strategy {
     @Override
     public void doAction() {
-        System.out.println("add-builder version 1.0.0-SNAPSHOT. (c) Henrik Teinelund 2018.");
-        System.out.println("");
+        MavenXpp3Reader reader = new MavenXpp3Reader();
+        Model model = null;
+        try {
+            model = reader.read(new FileReader("pom.xml"));
+            String version = model.getVersion();
+            System.out.println("add-builder version " + version + ". (c) Henrik Teinelund 2018.");
+            System.out.println("");
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (XmlPullParserException e) {
+            e.printStackTrace();
+        }
     }
 }
